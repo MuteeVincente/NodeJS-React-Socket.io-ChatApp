579af9c250cc8037229a9bda0167567ae7feb27d
MuteeVincente <vinniemutee@gmail.com>
2023-08-29 01:28:26 +0300

    index.js

--- server/index.js
+++ server/index.js
@@ -1,18 +1,27 @@
-const express  = require('express');
+const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
-
-const PORT = process.env.PORT || 5000
 const router = require('./router');
-const app = express();
+const cors = require('cors'); // Import the 'cors' middleware
 
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+app.use(cors()); // Invoke the 'cors' middleware here
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connection', (socket) => {
-    console.log('We have a Connection!!!');
 
-    socket.on('disconnect' ,() => console.log('User had left!!!'));
+io.on('connection', (socket) => {
+  console.log('We have a connection!!!');
+
+  socket.on('disconnect', () => {
+    console.log('User had left!!!');
+  });
 });
+
 app.use(router);
-server.listen(PORT, () => {console.log(`Server started at ${PORT}`);});
+
+server.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
+});
