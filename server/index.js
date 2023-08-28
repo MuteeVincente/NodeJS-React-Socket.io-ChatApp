c725d2ec8a229eef106ff8e78b4c747a93c17ce5
MuteeVincente <vinniemutee@gmail.com>
2023-08-28 17:24:13 +0300

    Socket.on code

--- server/index.js
+++ server/index.js
@@ -8,5 +8,11 @@ const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
+
+io.on('connection', (socket) => {
+    console.log('We have a Connection!!!');
+
+    socket.on('disconnect' ,() => console.log('User had left!!!'));
+});
 app.use(router);
 server.listen(PORT, () => {console.log(`Server started at ${PORT}`);});
