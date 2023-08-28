aebb72e80f225ed206dce24597011ce61a5016e5
MuteeVincente <vinniemutee@gmail.com>
2023-08-28 17:18:50 +0300

    Setting up routes and fetching response on the port

--- server/index.js
+++ server/index.js
@@ -3,9 +3,10 @@ const socketio = require('socket.io');
 const http = require('http');
 
 const PORT = process.env.PORT || 5000
+const router = require('./router');
 const app = express();
 
 const server = http.createServer(app);
 const io = socketio(server);
-
+app.use(router);
 server.listen(PORT, () => {console.log(`Server started at ${PORT}`);});
